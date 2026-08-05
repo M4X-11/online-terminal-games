bb2159ff428e2c89ab008cbbd61394e9381fe55b
M4X-11 <maxgraner10@gmail.com>
2026-08-05 14:19:29 -0400

    fix

--- src/client/data.h
+++ src/client/data.h
@@ -1,9 +1,14 @@
 
 
-typedef struct{
+#ifndef ADDRESS_H
+#define ADDRESS_H
+
+typedef struct {
     char ip[20];
     int port;
-}Address;
-Address addr;
+} Address;
 
+extern Address addr;
 int startConnection();
+#endif
+
