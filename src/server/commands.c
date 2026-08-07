d0d0d7487ec1f7dab4b57148fd22422b6b9cd6fd
M4X-11 <maxgraner10@gmail.com>
2026-08-07 05:08:56 -0400

    kick test, forgot returnn

--- src/server/commands.c
+++ src/server/commands.c
@@ -48,7 +48,8 @@ int cmd(char** args) {
     }
     if (strcmp(args[0], "kick") == 0) { 
         if (args[1] != NULL){printf("no player id given\n"); return 0;}
-        int numid = args[1] - '0';
+        //int numid = args[1] - '0';
+        int numid = atoi(args[1]);
         disconnectPlayer(numid);
     }
     
