28d192943d342c4dcbef3ce1eeb9a032c5657702
M4X-11 <maxgraner10@gmail.com>
2026-08-18 14:06:58 -0400

    restart cmdddd

--- src/server/snake/loop.c
+++ src/server/snake/loop.c
@@ -240,7 +240,7 @@ int SnakeGameLoop()
                 header.length = sizeof(packet);
                 send_all(game.players[i].socket, &header, sizeof(header));
                 send_all(game.players[i].socket, &packet, sizeof(packet));
-                printf("packet sent to player[%d]\n", i);
+                //printf("packet sent to player[%d]\n", i);
             }
         
         }
