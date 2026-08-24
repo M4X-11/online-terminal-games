5186c60359d65ad8f08a63dd8c58d3037f707d2d
M4X-11 <maxgraner10@gmail.com>
2026-08-24 09:32:06 -0400

    template-game added

--- build/CMakeFiles/game_server.dir/games/template-game/server/template-game.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/game_server.dir/games/template-game/server/template-game.c.o: \
- /home/kali/Desktop/otgAPI_test/otg/games/template-game/server/template-game.c \
- /usr/include/stdc-predef.h \
- /home/kali/Desktop/otgAPI_test/otg/games/template-game/server/../../../src/engineAPI.h \
- /usr/lib/gcc/x86_64-linux-gnu/15/include/stddef.h
